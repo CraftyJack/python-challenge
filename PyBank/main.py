3ec19d82d0e04b5b9903ff8ddce042098402b9d3
Jack Craft <CraftyJack@users.noreply.github.com>
2020-05-20 21:42:32 -0400

    commented out a debug print

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,10 +21,11 @@ with open(budget_csv) as csvfile:
     # Read the header row first
     csv_header = next(csvfile)
     # print it out to prove we found the right file.
-    print(f"Header: {csv_header}")
+    # print(f"Header: {csv_header}")
 # this csv has two columns.
 # the first column is the month (MMM-YYYY).
 # the second column is the profit/loss for that month.
+# read in each row
     for row in csvreader:
         vMonths = vMonths + 1
         vTotalPL = vTotalPL + int(row[1])
@@ -49,20 +50,3 @@ print(f"Total: ${vTotalPL}")
 print(f"Average  Change: ${round(vDeltaTotal / (vMonths -1),2)}")
 print(f"Greatest Increase in Profits: {vDeltaPLMax[0]} (${vDeltaPLMax[1]})")
 print(f"Greatest Decrease in Profits: {vDeltaPLMin[0]} (${vDeltaPLMin[1]})")
-
-# we want to know:
-
-#   * The total number of months included in the dataset
-# I am assuming each row is one month.
-
-#   * The net total amount of "Profit/Losses" over the entire period
-# This is calculated by adding together all of the values from column 2
-
-#   * The average of the changes in "Profit/Losses" over the entire period
-# This is the average of row n - row (n-1)
-
-#   * The greatest increase in profits (date and amount) over the entire period
-# This is the maximum value for row n - row (n-1)
-
-#   * The greatest decrease in losses (date and amount) over the entire period
-# This is the minimum value for row n - row (n-1)
